b256e4234c374360d8ba2e20940792ed365706b8
Fausto G. Cintra <professor@faustocintra.com.br>
2023-08-09 13:57:36 +0000

    (09/08) Configuração inicial do projeto

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ps-mat-2023-2
 Repositório da disciplina Eletiva II - Programação de Scripts, 5º semestre ADS matutino Fatec Franca 2023/2
+
+## Para criar o projeto
+npx @aka-demy/create-express-app
+
+## Para atualizar os pacotes e remover as vulnerabilidades
+npm audit fix --force
---
+++ back-end/src/app.js
@@ -0,0 +1,21 @@
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+
+// Importa o client do Prisma para fazer a conexão ao BD
+import prisma from './database/client.js'
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+export default app;
